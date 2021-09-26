d7248b4f53782d6b62c09ec989a55c6db91826b4
Bably <bablyd15@gmail.com>
2021-09-26 22:48:40 +0530

    cards added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ function App() {
             <Button color="primary" text="Customized Button" />
             <Button color="danger" text="Test Button" />
             <Button color="warning" text="Submit Button" />
-            <Card />
+            <Button color="info" text="Demo Button" />
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <Card
+                            title="first card"
+                            text="this is the first card"
+                        />
+                    </div>
+                </div>
+                <div className="col-4">
+                    <Card title="second card" text="this is the second card" />
+                </div>
+                <div className="col-4">
+                    <Card title="third card" text="this is the first card" />
+                </div>
+            </div>
+
             <Footer />
         </div>
     );
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,9 +1,15 @@
-const Card = () => {
+import Button from "./Button";
+const Card = (props) => {
+    const { title, text } = props;
+
     return (
-        <div>
-            <card>This is a card</card>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="title">{title}</h5>
+                <p class="text">{text}</p>
+            </div>
+            <Button color="primary" text="submit" />
         </div>
     );
 };
-
 export default Card;
